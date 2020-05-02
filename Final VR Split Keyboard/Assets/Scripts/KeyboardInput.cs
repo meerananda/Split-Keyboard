18c22fd817ed3e38e17830cd76ed21b6b17bb562
meerananda <meerananda91@gmail.com>
2020-05-02 11:37:04 -0400

    Added Unity project files

---
+++ Final VR Split Keyboard/Assets/Scripts/KeyboardInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyboardInput : MonoBehaviour
+{
+
+    string word = null;
+    int wordIndex = 0;
+    string alpha;
+    public Text inputText = null;
+
+    public void alphabetFunction(string alphabet)
+    {
+        wordIndex++;
+        word = word + alphabet;
+        inputText.text = word;
+
+    }
+}
