f490ede0a1bed2b9a35b75585ac998a30442c0b7
meerananda <meerananda91@gmail.com>
2020-05-03 12:26:18 -0400

    Added backspace and SpliT9 keyboard

--- Final VR Split Keyboard/Assets/Scripts/KeyboardInput.cs
+++ Final VR Split Keyboard/Assets/Scripts/KeyboardInput.cs
@@ -7,15 +7,43 @@ public class KeyboardInput : MonoBehaviour
 {
 
     string word = null;
-    int wordIndex = 0;
-    string alpha;
+    int wordIndex = -1;
+    string alpha = null;
+    string alpha2 = null;
+    char[] inputChar = new char[30];
+    //public Text index = null;
     public Text inputText = null;
 
     public void alphabetFunction(string alphabet)
     {
-        wordIndex++;
-        word = word + alphabet;
-        inputText.text = word;
+        //wordIndex++;
+        //word = word + alphabet;
+        //inputText.text = word;
 
+        wordIndex++;
+        char[] keepChar = alphabet.ToCharArray();
+        inputChar[wordIndex] = keepChar[0];
+        alpha = inputChar[wordIndex].ToString();
+        word = word + alpha;
+        inputText.text = word;
+        //index = wordIndex.ToString();
+    }
+
+    public void backspaceFunction(string alphabet)
+    {
+        if (wordIndex > 0)
+        {
+            wordIndex--;
+            alpha2 = null;
+
+            for (int i = 0; i < wordIndex + 1; i++)
+            {
+                alpha2 = alpha2 + inputChar[i].ToString();
+            }
+
+            word = alpha2;
+            inputText.text = word;
+
+        }
     }
 }
---
+++ Final VR Split Keyboard/Assets/Scripts/T9Hover.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class T9Hover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+
+
+    public GameObject CanvasABC;
+
+    void Start()
+    {
+        //image = GetComponent<Image>();
+    }
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        OnHoverEnter();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        OnHoverExit();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        OnClick();
+    }
+
+    void OnHoverEnter()
+    {
+        CanvasABC.SetActive(true);
+    }
+
+    void OnHoverExit()
+    {
+        CanvasABC.SetActive(false);
+    }
+
+    void OnClick()
+    {
+        //image.color = Color.blue;
+    }
+
+}
